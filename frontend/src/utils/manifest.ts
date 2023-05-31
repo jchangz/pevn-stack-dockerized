bf5e31daf55c9e09a95997f59741dcf7d7e25a96
Jian Chang <45157140+jchangz@users.noreply.github.com>
2023-05-31 02:33:01 -0600

    refactor(fe): migrate parseManifest to utils

---
+++ frontend/src/utils/manifest.ts
@@ -0,0 +1,15 @@
+import fs from "fs/promises"
+import path from "path"
+
+const environment = process.env.NODE_ENV
+
+const parseManifest = async () => {
+  if (environment !== "PROD") return {}
+
+  const manifestPath = path.join(path.resolve(), "dist", "manifest.json")
+  const manifestFile = await fs.readFile(manifestPath)
+// @ts-ignore
+  return JSON.parse(manifestFile)
+}
+
+export default parseManifest
