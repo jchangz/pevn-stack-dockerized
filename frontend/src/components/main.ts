057659ea10f2fec0e7695790824613fa3f7d46b6
Jian Chang <45157140+jchangz@users.noreply.github.com>
2023-05-31 21:12:48 -0600

    refactor(fe): move main.ts, update controller

--- frontend/src/components/main.ts
+++ frontend/src/components/main.ts
@@ -1,17 +1,17 @@
-import "./style.css"
-import typescriptLogo from "./typescript.svg"
+import "../style.css"
+import typescriptLogo from "../assets/typescript.svg"
 import viteLogo from "/vite.svg"
 import { setupCounter } from "./counter.ts"
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <a class="inline-block" href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
+    <a class="inline-block" href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1 class="font-bold text-6xl">Vite + TypeScript</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
@@ -26,6 +26,8 @@ fetch(`/api/test`)
   .then((res) => {
     const container = document.createElement("div")
     container.innerHTML = JSON.stringify(res)
+    container.className =
+      "font-bold p-4 bg-black font-mono text-white rounded-md"
     document.querySelector<HTMLDivElement>("#app")!.appendChild(container)
   })
 
--- frontend/src/controller/homepageController.ts
+++ frontend/src/controller/homepageController.ts
@@ -1,25 +1,13 @@
-import fs from "fs/promises"
-import path from "path"
+import parseManifest from "../utils/manifest.ts"
 
 const environment = process.env.NODE_ENV
 
-const parseManifest = async () => {
-  if (environment !== "PROD") return {}
-
-  const manifestPath = path.join(path.resolve(), "dist", "manifest.json")
-  const manifestFile = await fs.readFile(manifestPath)
-
-  return JSON.parse(manifestFile)
-}
-
-const data = {
-  environment,
-  manifest: await parseManifest(),
-  script: "src/main.ts",
-}
-
 const homeView = async (req, res) => {
-  res.render("index.ejs", data)
+  res.render("index.ejs", {
+    environment,
+    manifest: await parseManifest(),
+    script: "src/components/main.ts",
+  })
 }
 
 export { homeView }
